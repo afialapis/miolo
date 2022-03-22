26e06cf0700aa5bd5264432a7f8e047a64b09cc6
afialapis <devel@afialapis.com>
2022-03-23 00:33:25 +0100

    calustra update with get_uid

--- packages/miolo/src/server/routes/crud.js
+++ packages/miolo/src/server/routes/crud.js
@@ -5,8 +5,22 @@ async function init_crud_router (app, conn, config) {
   if (! config) {
     return undefined
   }
+
+  const routerOptions= {
+    body_field: undefined,
+    get_uid: (ctx) => {
+      try {
+        return ctx.state.user.id
+      } catch(e) {}
+      let uid= ctx.headers['user-id']
+      if (uid!=undefined) {
+        return uid
+      }      
+      return undefined
+    }
+  }
   
-  const router= await calustraRouter(conn, config.routes, config.path, 'public', {body_field: undefined})
+  const router= await calustraRouter(conn, config.routes, config.path, 'public', routerOptions)
   app.use(router.routes())
   
 }
