c02206ba09c397377402f8633ffc6838d2a32d34
afialapis <devel@afialapis.com>
2022-03-27 01:14:55 +0100

    update calustra api

--- packages/miolo-demo/src/server/db/routes/crud.js
+++ packages/miolo-demo/src/server/db/routes/crud.js
@@ -1,13 +1,13 @@
 module.exports= {
   prefix: '/crud',
+  authUser: {
+    require: 'read-only',
+    action: 'redirect',
+    redirect_url: '/'
+  },  
   routes: [
     {
       name: 'todos',
-      authUser: {
-        require: 'read-only',
-        action: 'redirect',
-        redirect_url: '/'
-      },
       options: {
         useDates: false,
         //checkBeforeDelete: ["edition.agent_id"],
