a11202b030019780af38d138d7805141c32b89b7
afialapis <devel@afialapis.com>
2023-04-20 16:56:37 +0200

    fix

--- src/cli-react/ssr/AppServer.mjs
+++ src/cli-react/ssr/AppServer.mjs
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { StaticRouter } from 'react-router-dom/server'
+import { StaticRouter } from 'react-router-dom/server.js'
 import AppContext from './context/AppContext.mjs'
 
 
