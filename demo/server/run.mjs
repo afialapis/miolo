3c782c4279a97f25d97c577922295f5e4415c27a
afialapis <devel@afialapis.com>
2025-04-10 16:00:34 +0200

    cleaning

--- demo/server/run.mjs
+++
@@ -1,9 +0,0 @@
-import { miolo_demo_server } from "./server.mjs";
-
-let authType= 'credentials'
-try {
-  authType= process.argv[2].replace('--', '')
-} catch(_) {}
-
-console.log(`[miolo-demo-${authType}][server] Starting...`)
-miolo_demo_server(authType)
