3ff82d826877439ae20fb474bd888e84d6f2747c
afialapis <devel@afialapis.com>
2025-04-20 20:35:16 +0200

    miolo cron server

---
+++ src/server/server-cron.mjs
@@ -0,0 +1,39 @@
+import { init_config }                    from './config/index.mjs'
+import { init_context_middleware }         from './middleware/context/index.mjs'
+import { init_cron }                       from './engines/cron/index.mjs'
+
+export async function miolo_cron(sconfig) {
+
+  const app = {
+    use: () => {},
+  }
+
+  // Init some pieces
+  const config = init_config(sconfig)
+  
+  // attach to app some custom miolo methods
+  init_context_middleware(app, config)
+
+
+  // Init cron (will not start jobs yet)
+  init_cron(app, config?.cron)
+    
+  // Util callbacks
+  app.start = async () => {
+    // Init and reset db connection
+    await app.context.miolo.db.init_connection()
+    app.cron.start()
+  }
+  
+  app.stop = async () => {
+    app.context.miolo.db.drop_connections()
+    app.cron.stop()
+  }
+
+  app.restart = async () => {
+    await app.stop()
+    await app.start()
+  }
+
+  return app
+}
