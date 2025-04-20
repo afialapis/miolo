98d24d83ac05d62afb484fa0474d3e1094c2ec25
afialapis <devel@afialapis.com>
2025-04-20 20:34:58 +0200

    miolo_cron server

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -1,4 +1,5 @@
 import { miolo } from './server.mjs'
+import { miolo_cron } from './server-cron.mjs'
 import { init_emailer_transporter as miolo_emailer} from './engines/emailer/index.mjs'
 import { init_logger as miolo_logger } from './engines/logger/index.mjs'
 import { init_parser as miolo_parser } from './engines/parser/index.mjs'
@@ -8,6 +9,7 @@ import { getConnection as miolo_db_connection} from 'calustra/conn-postgres'
 
 export {
   miolo,
+  miolo_cron,
   miolo_emailer,
   miolo_logger,
   miolo_parser,
