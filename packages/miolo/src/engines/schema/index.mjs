179bc9e1bdb1f0192306dad5cdfab52e707d01d3
afialapis <devel@afialapis.com>
2025-11-12 13:20:37 +0100

    fix with_miolo_schema

--- packages/miolo/src/engines/schema/index.mjs
+++ packages/miolo/src/engines/schema/index.mjs
@@ -3,38 +3,38 @@ import Joi from "joi"
 export function with_miolo_schema(fn, schema) {
   return async function (ctx, params) {
     let error
-    let parsedParams = params
     
+    // Check schema is actually a schema
+    if ((!schema) || (! Joi.isSchema(schema))) {
+      error = `Expecting schema for ${fn.name} but something else was found (${typeof schema})`
+      ctx.miolo.logger.error(`[validation][${fn.name}] ${error}`)
+      throw new Error(error)
+    }
+    
+    // perform validation
+    let v
     try {
-      // Check schema is actually a schema
-      if ((!schema) || (! Joi.isSchema(schema))) {
-        error = `Expecting schema for ${fn.name} but something else was found (${typeof schema})`
-        ctx.miolo.logger.error(`[validation][${fn.name}] ${error}`)
-        throw new Error(error)
-      }
-      
-      // perform validation
-      const v = schema.validate(params)
-      if (v?.error) {
-        error = `Schema invalidated data for ${fn.name}: ${v.error}\n${v.error.annotate(true)}`
-        ctx.miolo.logger.error(`[validation][${fn.name}] ${error}`)
-        throw new Error(error)
-      }
-
-      // check parsed value is ok
-      if (!v?.value) {
-        error = `Schema returned unknown result for ${fn.name}: ${JSON.stringify(v)}`
-        ctx.miolo.logger.error(`[validation][${fn.name}] ${error}`)
-        throw new Error(error)
-      }
-
-      parsedParams = v.value
+      v = schema.validate(params)
     } catch(uerror) {
       error = `Unexpected error validating data for ${fn.name}: ${uerror?.message || uerror}`
       ctx.miolo.logger.error(`[validation][${fn.name}] ${error}`)
       throw new Error(error)
     }
 
-    return await fn(ctx, parsedParams)
+    // raise validation errors
+    if (v?.error) {
+      error = `Schema invalidated data for ${fn.name}: ${v.error}\n${v.error.annotate(true)}`
+      ctx.miolo.logger.error(`[validation][${fn.name}] ${error}`)
+      throw new Error(error)
+    }
+
+    // check parsed value is ok
+    if (!v?.value) {
+      error = `Schema returned unknown result for ${fn.name}: ${JSON.stringify(v)}`
+      ctx.miolo.logger.error(`[validation][${fn.name}] ${error}`)
+      throw new Error(error)
+    }
+    
+    return await fn(ctx, v.value)
   }
 }
