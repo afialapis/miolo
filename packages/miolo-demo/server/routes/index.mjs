a03d5c19ca61cf3a5a1303d4a343fc53adeb2003
afialapis <devel@afialapis.com>
2026-03-04 14:47:06 +0100

    no demo

--- packages/miolo-demo/server/routes/index.mjs
+++
@@ -1,8 +0,0 @@
-import crud from './crud.mjs'
-import queries from './queries.mjs'
-
-export default {
-  bodyField: undefined,
-  crud: crud,
-  queries: queries
-}
--- packages/miolo-demo/server/server.mjs
+++
@@ -1,9 +0,0 @@
-import {miolo} from 'miolo'
-import makeConfig from './config.mjs'
-
-export default async function miolo_demo_server () {  
-  const app = await miolo(makeConfig)
-  await app.start()
-
-  return app
-}
