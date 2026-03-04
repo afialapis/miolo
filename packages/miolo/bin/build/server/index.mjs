5e0a296cb795bc23d721f5a70ba7fd869f59d95d
afialapis <devel@afialapis.com>
2026-03-04 15:06:47 +0100

    fix for build

--- packages/miolo/bin/build/server/index.mjs
+++ packages/miolo/bin/build/server/index.mjs
@@ -1,52 +1,52 @@
+import path from "node:path"
+import chokidar from "chokidar"
+import { miolo_bundle } from "./bundle.mjs"
+import { miolo_fix_prod_build } from "./fix.mjs"
+import { miolo_build_options_for_server } from "./options.mjs"
 
-import chokidar from 'chokidar'
-import path from 'path'
-import {miolo_build_options_for_server} from './options.mjs'
-import {miolo_bundle} from './bundle.mjs'
-import {miolo_fix_prod_build} from './fix.mjs'
-
-async function _miolo_build_server(appName, pkgPath, srvEntry, srvDest, srvExt, watch= false) {
-  const [esmnInputOptions, esmnOutputs, outputFile] = await miolo_build_options_for_server(appName, pkgPath, srvEntry, srvDest, srvExt)
+async function _miolo_build_server(appName, pkgPath, srvEntry, srvDest, srvExt, watch = false) {
+  const [esmnInputOptions, esmnOutputs, outputFile] = await miolo_build_options_for_server(
+    appName,
+    pkgPath,
+    srvEntry,
+    srvDest,
+    srvExt
+  )
   await miolo_bundle(appName, pkgPath, esmnInputOptions, esmnOutputs, watch)
   await miolo_fix_prod_build(appName, outputFile)
 }
 
-
 export async function miolo_build_server(appName, pkgPath, config, srvEntry, srvDest, srvExt) {
-  
   // const watch = config.build.dev.watcher?.enabled === true
   const watch = false
 
   await _miolo_build_server(appName, pkgPath, srvEntry, srvDest, srvExt, watch)
 
   if (watch) {
-    const srcFolder = path.join(pkgPath, 'src')
-    const buildFolder = path.join(pkgPath, 'build')
+    const srcFolder = path.join(pkgPath, "src")
+    const buildFolder = path.join(pkgPath, "build")
 
     const watcher = chokidar.watch(srcFolder, {
       ignored: buildFolder
     })
 
     try {
-      watcher.on('ready', () => {
+      watcher.on("ready", () => {
         let watched = Object.keys(watcher.getWatched())
         try {
           watched = watched[0]
-        } catch(_) {}
+        } catch (_) {}
 
         console.log(`[${appName}][build] Bundling in watch mode (${watched})`)
-        
-        watcher.on('all', () => {  
-          _miolo_build_server(appName, pkgPath, srvEntry, srvDest, watch).then(( )=> {
 
-          })
+        watcher.on("all", () => {
+          _miolo_build_server(appName, pkgPath, srvEntry, srvDest, watch).then(() => {})
         })
       })
-    } catch(_error) {
+    } catch (_error) {
       watcher.close().then(() => {
         console.log(`[${appName}][build] Bundling watcher closed!`)
       })
     }
   }
-
 }
--- packages/miolo/bin/build/server/options.mjs
+++ packages/miolo/bin/build/server/options.mjs
@@ -1,75 +1,78 @@
-import path from 'node:path'
-import json from '@rollup/plugin-json'
-import {babel} from '@rollup/plugin-babel'
+import path from "node:path"
+import { babel } from "@rollup/plugin-babel"
+import commonjs from "@rollup/plugin-commonjs"
+import json from "@rollup/plugin-json"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import replace from "@rollup/plugin-replace"
+import tailwindcss from "@tailwindcss/postcss"
+import autoprefixer from "autoprefixer"
 //import babel from '@rollup/plugin-swc'
-import externals from 'rollup-plugin-node-externals'
-import replace from '@rollup/plugin-replace'
-import {nodeResolve} from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import scss from 'rollup-plugin-postcss'
-import tailwindcss from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
+import externals from "rollup-plugin-node-externals"
+import scss from "rollup-plugin-postcss"
+import { getRollupPluginForResolvingAliases } from "./aliases.mjs"
+import BABEL_CONFIG from "./babel.config.js"
+import { miolo_build_banner } from "./banner.mjs"
 
-import { miolo_build_banner } from './banner.mjs'
-import { getRollupPluginForResolvingAliases } from './aliases.mjs'
-import BABEL_CONFIG from './babel.config.js'
+const NODE_ENV = process.env?.NODE_ENV || "production"
 
-const NODE_ENV = process.env?.NODE_ENV || 'production'
-
-export async function miolo_build_options_for_server(appName, pkgPath, srvEntry, srvDest, srvExt, bundleDeps= true) {
+export async function miolo_build_options_for_server(
+  appName,
+  pkgPath,
+  srvEntry,
+  srvDest,
+  srvExt,
+  bundleDeps = true
+) {
   const input = path.join(pkgPath, srvEntry)
   const outputFile = path.join(pkgPath, srvDest, `${appName}.${srvExt}`)
-  
-  const inputOptions= {
+
+  const inputOptions = {
     input,
     plugins: [
       replace({
         preventAssignment: true,
-        'global.process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-        'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+        "global.process.env.NODE_ENV": JSON.stringify(NODE_ENV),
+        "process.env.NODE_ENV": JSON.stringify(NODE_ENV)
       }),
-      babel(BABEL_CONFIG), 
+      babel(BABEL_CONFIG),
       commonjs({
-        esmExternals: true
-      }),      
+        esmExternals: false
+      }),
       ...getRollupPluginForResolvingAliases(pkgPath),
       json(),
       externals({
-        packagePath: path.join(pkgPath, 'package.json'),
+        packagePath: path.join(pkgPath, "package.json"),
         deps: !bundleDeps,
         peerDeps: !bundleDeps
       }),
       nodeResolve({
         rootDir: pkgPath,
-        exportConditions: ['node'],
+        exportConditions: ["node"]
       }),
 
       scss({
         extract: true,
-        plugins: [
-          tailwindcss({base: pkgPath}),
-          autoprefixer()
-        ],
+        plugins: [tailwindcss({ base: pkgPath }), autoprefixer()],
         use: {
           sass: {
-            silenceDeprecations: ['legacy-js-api'],
+            silenceDeprecations: ["legacy-js-api"]
           }
-        }        
+        }
       })
     ],
-    onwarn : (warning) => {
-      if (warning.code === 'CIRCULAR_DEPENDENCY')    return;
+    onwarn: (warning) => {
+      if (warning.code === "CIRCULAR_DEPENDENCY") return
       console.warn(warning.message)
     }
   }
-  
+
   const banner = await miolo_build_banner(pkgPath)
-  const outputs= [
+  const outputs = [
     {
       file: outputFile,
       inlineDynamicImports: true,
-      format: 'esm',
-      exports: 'named',
+      format: "esm",
+      exports: "named",
       banner
     }
   ]
